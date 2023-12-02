af5c24b1872e056294e11ab72eced82644daafb6
Kevin Glowacz <kjg@github.com>
2023-12-02 13:45:26 -0600

    D1P1

---
+++ day1/part1.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+file = File.join(File.dirname(__FILE__), ARGV[0] || 'example.txt')
+
+total = 0
+File.open(file).each do |l|
+  number = l.match(/\d/)[0] << l.reverse.match(/\d/)[0]
+  total += number.to_i
+end
+puts total
